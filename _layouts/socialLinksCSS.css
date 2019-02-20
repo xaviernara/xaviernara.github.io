1fa15135b2ff29c863104345a1da37b277d5cc28
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2019-02-19 20:12:26 -0800

    made socail link css file

---
+++ _layouts/socialLinksCSS.css
@@ -0,0 +1,32 @@
+		.fa {
+			  padding: 20px;
+			  font-size: 30px;
+			  width: 30px;
+			  text-align: center;
+			  text-decoration: none;
+			  margin: 5px 2px;
+			  border-radius: 50%;
+		}
+		.fa:hover {
+			opacity: .5;
+		}
+		
+		.fa-linkedin {
+		  background: #007bb5;
+		  color: white;
+		}
+		
+		.fa-skype {
+		  background: #00aff0;
+		  color: white;
+		}
+		
+		.fa-facebook {
+		  background: #3B5998;
+		  color: white;
+		}
+		
+		.fa-twitter {
+		  background: #55ACEE;
+		  color: white;
+		}
