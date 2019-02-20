54a021732ff64c4942683e937921871ab69e8153
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2019-02-19 19:52:23 -0800

    adding bio and social links 2 php files

---
+++ _layouts/bioFromWebsite.php
@@ -0,0 +1,15 @@
+<div class="bio">
+			<p>
+				" Xavier Richardson is a recent Purdue graduate with a degree in Computer Engineering, where he studied electrical engineering, and computer science. 
+				During Xavier's time at university he became the team lead for the Facial Recognition Attendance App. Team and gained a passion for both 
+				Android and Full Stack development. Xavier built a database that houses user-names, emails, passwords, and pictures using MySQL.
+				Xavier utilized PHP to construct the server that â€‹manipulates, analyzes and creates new data based on HTTP requests from the app's website and app UI.<br> 
+				
+				Xavier became the Head Programmer for the university's Astronomy Research Team, where he developed Python code to collect and categorize
+				differentiated temperature dependences of high and low dark current from a closed capture telescope.<br>
+			
+				Xavier showcased his hardware skills by designing a single core processor, where he implemented use of Altera? ?Quartus and Model SIM to test, design 
+				and implement? ?the micro-architecture of the single core processor. He utilized Intel’s Mars M.I.P.S. ?Industry Standard Architecture? 
+				to write assembly language instructions to the processor and simulate runtime. "
+			</p>
+	</div>
