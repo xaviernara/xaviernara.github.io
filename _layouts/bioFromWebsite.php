4fdb73e836894e91d5bcc1f4693f796c5a8d7a21
Xavier Nara <32371616+xaviernara@users.noreply.github.com>
2019-08-10 05:53:50 -0700

    Update bioFromWebsite.php

--- _layouts/bioFromWebsite.php
+++ _layouts/bioFromWebsite.php
@@ -1,6 +1,6 @@
 <div class="bio">
 			<p>
-				" Xavier Richardson is a recent Purdue graduate with a degree in Computer Engineering, where he studied electrical engineering, and computer science. 
+				Xavier Richardson is a recent Purdue graduate with a degree in Computer Engineering, where he studied electrical engineering, and computer science. 
 				During Xavier's time at university he became the team lead for the Facial Recognition Attendance App. Team and gained a passion for both 
 				Android and Full Stack development. Xavier built a database that houses user-names, emails, passwords, and pictures using MySQL.
 				Xavier utilized PHP to construct the server that â€‹manipulates, analyzes and creates new data based on HTTP requests from the app's website and app UI.<br> 
@@ -10,6 +10,6 @@
 			
 				Xavier showcased his hardware skills by designing a single core processor, where he implemented use of Altera? ?Quartus and Model SIM to test, design 
 				and implement? ?the micro-architecture of the single core processor. He utilized Intel’s Mars M.I.P.S. ?Industry Standard Architecture? 
-				to write assembly language instructions to the processor and simulate runtime. "
+				to write assembly language instructions to the processor and simulate runtime. 
 			</p>
 	</div>
